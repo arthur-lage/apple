06cf2f03eebc6570b70bbac6b19b00ea480b272c
Arthur Lage <arthurlage2006@gmail.com>
2022-03-05 21:16:30 -0300

    shopping cart

---
+++ scripts/dropdown.js
@@ -0,0 +1,6 @@
+const cartButton = document.querySelector("#shopping-cart-button");
+const cartDropdown = document.querySelector(".dropdown");
+
+cartButton.addEventListener("click", () => {
+  cartDropdown.classList.toggle("active");
+});
