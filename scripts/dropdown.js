350a8a2a94f9243d669dcad828356c87884c963f
Arthur Lage <arthurlage2006@gmail.com>
2022-03-15 19:34:14 -0300

    final media queries

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,6 +1,21 @@
-const cartButton = document.querySelector(".shopping-cart-button");
-const cartDropdown = document.querySelector(".dropdown");
+const cartButtons = document.querySelectorAll(".shopping-cart-button");
+const cartDropdowns = document.querySelectorAll(".dropdown");
+const searchOverlay = document.querySelector(".search-overlay");
 
-cartButton.addEventListener("click", () => {
-  cartDropdown.classList.toggle("active");
+cartButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (screen.width < 834) {
+      searchOverlay.classList.toggle("active");
+
+      if (searchOverlay.classList.contains("active")) {
+        document.body.style.overflowY = "hidden";
+      } else {
+        document.body.style.overflowY = "auto";
+      }
+    }
+
+    cartDropdowns.forEach((dropdown) => {
+      dropdown.classList.toggle("active");
+    });
+  });
 });
